1dffcae5144c9aa2934859ad7fc74708e35d7282
erynder-z <stfn.bgr@gmail.com>
2022-12-13 23:08:08 +0100

    Create a simple rest api

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'John Doe',
+  },
+  2: {
+    id: '2',
+    username: 'Max Power',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
